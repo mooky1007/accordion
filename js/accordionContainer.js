bec2aa929bf992cfb25bcff801148a49a1eb2bb9
mooky1007 <mooky1007@naver.com>
2023-06-26 13:23:47 +0900

    update method

--- js/accordionContainer.js
+++ js/accordionContainer.js
@@ -1,24 +1,28 @@
 class Accordion {
-    constructor(el) {
-        this.el = el;
+    constructor(el, config) {
+        this.el = document.querySelector(el);
+        this.open = config?.open || false;
+        this.multi = config?.multi || false;
         this.init();
     }
 
     init() {
-        this.items = [...this.el.querySelectorAll('.accordion_item')].map(item => new AccordionItem(item));
-        this.el.addEventListener('accOpen', () => this.closeAll());
+        this.items = [...this.el.querySelectorAll('.accordion_item')].map(item => new AccordionItem(item, this.el));
+        if(this.open) this.openAll();
+        if(!this.multi) this.el.addEventListener('accOpen', () => this.closeAll());
     }
 
-    closeAll() {
-        this.items.forEach(item => item.active ? item.close() : null);
-    }
+    openNum(idx) { this.items[idx - 1].open(); }
+    closeNum(idx) { this.items[idx - 1].close(); }
+    openAll() { this.items.forEach(item => item.open()); }
+    closeAll() { this.items.forEach(item => item.active ? item.close() : null); }
 }
 
 class AccordionItem {
-    constructor(el) {
-        this.container = el.closest('.accordion_container');
+    constructor(el, parent) {
+        this.container = parent;
         this.el = el;
-        this.active = false;
+        this._active = false;
         this.title = this.el.querySelector('.accordion_title');
         this.content = this.el.querySelector('.accordion_content');
         this.style = this.el.style;
@@ -27,21 +31,30 @@ class AccordionItem {
         this.init();
     }
 
+    get active() {
+        return this._active;
+    }
+
+    set active(value) {
+        this._active = value;
+    }
+
     init() {
-        this.style.maxHeight = this.title.scrollHeight + 'px';
+        this.style.maxHeight = `${this.title.scrollHeight}px`;
         this.style.overflow = 'hidden';
         this.el.addEventListener('click', () => this.toggle());
+        if(this.el.classList.contains('active')) this.open();
     }
 
     open() {
         this.container.dispatchEvent(this.accOpen);
         this.active = true;
-        this.style.maxHeight = this.title.scrollHeight + this.content.scrollHeight + 'px';
+        this.style.maxHeight = `${this.title.scrollHeight + this.content.scrollHeight}px`;
     }
 
     close() {
         this.active = false;
-        this.style.maxHeight = this.title.scrollHeight + 'px';
+        this.style.maxHeight = `${this.title.scrollHeight}px`;
     }
 
     toggle() {
